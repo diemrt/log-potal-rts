0f2b3f3195d2a331794129a9d2ceb372c5631837
diego <diego.martignoni@abenergie.it>
2023-02-17 19:50:07 +0100

    fixing sidebar with solid

---
+++ src/components/SelectInput.Component.tsx
@@ -0,0 +1,19 @@
+import { useState } from "react";
+import LazyLoadingComponent from "./LazyLoading.Component";
+
+export default function SelectInputComponent(props: {lazyLoading: {renderCondition: boolean, skeleton: JSX.Element}, children: JSX.Element, popUpElement: JSX.Element}) {
+    let [isVisible, setVisibility] = useState(false);
+    let show = () => setVisibility(true);
+    let hide = () => setVisibility(false);
+
+    return (
+        <LazyLoadingComponent {...props.lazyLoading}>
+            <div>
+                <button onClick={isVisible ? hide : show}>
+                    {props.children}
+                </button>
+                {isVisible ? <div onClick={hide}>{props.popUpElement}</div> : <span></span>}
+            </div>
+        </LazyLoadingComponent>
+    )
+}
